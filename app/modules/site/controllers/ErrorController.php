ec1763871fe319da7bc19d89f5dfc460d27e7582
atnartur <atnartur2012@gmail.com>
2015-10-09 23:22:34 +0300

    - убраны лишние файлы

--- app/modules/site/controllers/ErrorController.php
+++
@@ -1,8 +0,0 @@
-<?
-namespace App\Modules\Site\Controllers;
-
-class ErrorController extends ControllerBase {
-    public function notFoundAction(){
-        $this->response->setStatusCode(404, 'Not Found');
-    }
-}
