b10658e1de8afe0ab6d1337ba240014aec53ac4c
atnartur <atnartur2012@gmail.com>
2015-10-09 19:55:39 +0300

    + config server sample

--- app/config/server_sample.php
+++ app/config/server_sample.php
@@ -1,4 +1,15 @@
-<?
+<?php
 $config_server = new \Phalcon\Config(array(
-
+    'api' => array(
+        'yandex_disk' => array(
+            'client_id' => '',
+            'client_secret' => '',
+            'ya_token' => '',
+            'base_dir' => ''
+        ),
+        'telegram' => array(
+            'api_key' => "",
+            "chat_ids" => array(1)
+        )
+    )
 ));
